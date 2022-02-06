bbd42c6f6d3aa967e42bd0c146ab3ca068b0371b
* <element9v@gmail.com>
2022-02-06 13:30:34 +0700

    fix

--- emotive/handler/scrape.sh
+++ emotive/handler/scrape.sh
@@ -4,12 +4,12 @@ lynx -dump ${1} >index
 ./emote.sh index
 cat >> scraped.html <<EOF
 <table width=100%>
-<td width=100% >${1}</td><tr>
-<td align=left width=100%><table>
-<td background="#00ff00" width=$(cat admiration.txt)>&nbsp;</td>
-<td bgcolor="#ffff20" width=$(cat ecstasy.txt)>&nbsp;</td>
-<td bgcolor="#ff2020" width=$(cat rage.txt)>&nbsp;</td>
-<td bgcolor="#6060ff" width=$(cat amazement.txt)>&nbsp;</td>
+<td>${1}</td><tr>
+<td align=left width=100%><table width=100%>
+<td bgcolor="#00ff00" width=$(cat admiration.txt)%>&nbsp;</td>
+<td bgcolor="#ffff20" width=$(cat ecstasy.txt)%>&nbsp;</td>
+<td bgcolor="#ff2020" width=$(cat rage.txt)%>&nbsp;</td>
+<td bgcolor="#6060ff" width=$(cat amazement.txt)%>&nbsp;</td>
 </table></td>
 <tr>
 </table><br>
