56da7c76ef8a9e2d38538b42916430e2c813f4d2
* <element9v@gmail.com>
2022-02-06 10:55:51 +0700

    normal

--- emotive/handler/emote.sh
+++ emotive/handler/emote.sh
@@ -12,4 +12,16 @@ awk -f sum.awk blah > rage.txt
 rm blah
 for emote in $(cat ../amazement.txt); do { grep ${emote} $msgfile | wc -l>>blah; }; done
 awk -f sum.awk blah > amazement.txt
+
+cat admiration.txt ecstasy.txt rage.txt amazement.txt >sum
+mult=$(awk '{ total += $1; count++ } END { print int(total *25/count)}' sum)
+
+for file in admiration.txt ecstasy.txt rage.txt amazement.txt; do {
+	val=$(cat $file)
+	echo $(( $val * $mult)) > $file
+}; done
+
+
+
 ./build.sh
+
--- emotive/handler/scrape.sh
+++ emotive/handler/scrape.sh
@@ -3,9 +3,9 @@ rm -f index.htm
 lynx -dump ${1} >index
 ./emote.sh index
 cat >> scraped.html <<EOF
-<table>
-<td >${1}</td><tr>
-<td align=left><table>
+<table width=100%>
+<td width=100% >${1}</td><tr>
+<td align=left width=100%><table>
 <td background="#00ff00" width=$(cat admiration.txt)>&nbsp;</td>
 <td bgcolor="#ffff20" width=$(cat ecstasy.txt)>&nbsp;</td>
 <td bgcolor="#ff2020" width=$(cat rage.txt)>&nbsp;</td>
